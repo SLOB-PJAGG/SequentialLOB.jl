4a8e9354f01c121f1da8dc2e4e5e8e03ba697f41
Michael <michael.j.g.za@gmail.com>
2019-12-27 20:27:34 +0200

    remove nu and self jump prob

--- src/reaction_diffusion_path.jl
+++ src/reaction_diffusion_path.jl
@@ -7,7 +7,6 @@ mutable struct SLOB
     L::Float64
     D::Float64
     σ::Float64
-    nu::Float64
     α::Float64
     source_term::SourceTerm
     x::Array{Float64, 1}
@@ -15,7 +14,7 @@ mutable struct SLOB
     Δt::Float64
 end
 function SLOB(num_paths::Int64, T::Int64, p₀::Float64,
-    M::Int64, β::Float64, L::Real, D::Float64, σ::Float64, nu::Float64,
+    M::Int64, β::Float64, L::Real, D::Float64, σ::Float64,
     α::Float64, source_term::SourceTerm)
     logger = FileLogger(Dict(Logging.Info => "info.log", Logging.Error => "error.log"), append=false)
     global oldglobal = global_logger(logger)
@@ -34,20 +33,20 @@ function SLOB(num_paths::Int64, T::Int64, p₀::Float64,
     Δx = L/M
     Δt = (Δx^2) / (2.0*D)
     return SLOB(num_paths, T, p₀, M, β,
-        L, D, σ, nu, α, source_term, x, Δx, Δt)
+        L, D, σ, α, source_term, x, Δx, Δt)
 end
 
 
 SLOB(dict) = SLOB(
     dict["num_paths"], dict["T"], dict["p₀"], dict["M"],
     dict["β"], dict["L"], dict["D"],
-    dict["σ"], dict["nu"], dict["α"], SourceTerm(dict["λ"], dict["μ"]))
+    dict["σ"], dict["α"], SourceTerm(dict["λ"], dict["μ"]))
 
 
 SLOB(;num_paths::Int64=1, T::Int64=100, p₀::Real=100.0,
     M::Int64=100, β::Real=1.0, L::Real=50.0, D::Real=4.0, σ::Real=1.0,
-    nu::Real=0.0, α::Real=1.0, λ::Real=1.0, μ::Real=0.5) =
-    SLOB(num_paths, T, p₀, M, β, L, D, σ, nu, α,
+    α::Real=1.0, λ::Real=1.0, μ::Real=0.5) =
+    SLOB(num_paths, T, p₀, M, β, L, D, σ, α,
     SourceTerm(λ, μ))
 
 
@@ -68,16 +67,16 @@ function (slob::SLOB)(seed::Int=-1)
     lob_densities = zeros(Float64, slob.M+1, time_steps + 1, slob.num_paths)
     P⁺s = ones(Float64, time_steps, slob.num_paths)
     P⁻s = ones(Float64, time_steps, slob.num_paths)
-    Ps = ones(Float64, time_steps, slob.num_paths)
+
     for path in 1:slob.num_paths
         Random.seed!(seeds[path])
         @info "path $path with seed $(seeds[path])"
         lob_densities[:, :, path], raw_price_paths[:, path],
             sample_price_paths[:, path], P⁺s[:, path],
-            P⁻s[:, path], Ps[:, path]  = dtrw_solver(slob)
+            P⁻s[:, path]  = dtrw_solver(slob)
     end
 
-    return lob_densities, raw_price_paths, sample_price_paths, P⁺s, P⁻s, Ps
+    return lob_densities, raw_price_paths, sample_price_paths, P⁺s, P⁻s
 end
 
 function get_time_steps(T::Int, Δt::Float64)
